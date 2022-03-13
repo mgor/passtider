019c8fbfa933d2e6e5e7bb94055888ede939c356
Mikael Göransson <github@mgor.se>
2022-03-13 22:27:04 +0000

    ops

--- passtider/__main__.py
+++ passtider/__main__.py
@@ -3,6 +3,4 @@ import sys
 from passtider import main, parse_available_times
 
 if __name__ == '__main__':
-    with open('result.html') as fd:
-        parse_available_times(fd.read())
-    #sys.exit(main())
+    sys.exit(main())
